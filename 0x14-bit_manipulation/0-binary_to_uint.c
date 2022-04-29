20354ec46d4ea73c600a4ce1ab1228df569c9adf
root <sylyac2000@gmail.com>
2022-04-29 01:04:44 -0700

    Updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,19 +7,31 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+
+	int len = 0, i;
 	unsigned int sum = 0;
 
-	for (sum = 0; *b; b++)
+	if (b == NULL)
+		return (sum);
+
+	/* find string length */
+	while (b[len] != '\0')
+		len++;
+	len -= 1;
+
+	/* iterate string and if '1' then multiply by power of 2 */
+	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
+	i = 0;
+	while (b[i] != '\0')
 	{
-		if (*b == '1')
-			sum = (sum << 1) | 1;
-		else if (*b == '0')
-			sum <<= 1;
-		else
-		{
-			sum = 0;
-			break;
-		}
+		if ((b[i] != '0') && (b[i] != '1'))
+			return (0);
+
+		if (b[i] == '1')
+			sum += (1 * (1 << len));
+		i++;
+		len--;
 	}
+
 	return (sum);
 }
