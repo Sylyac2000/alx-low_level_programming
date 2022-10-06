c359487d5e396c74702a5b771eb71ea33e13e01a
root <sylyac2000@gmail.com>
2022-10-05 22:58:08 -0700

    Write a function that searches for a value in a sorted array of integers using the Exponential search algorithm

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -2,6 +2,42 @@
 #include "search_algos.h"
 
 /**
+* do_binary_search - Linear search function
+* @array: Pointer to integer
+* @left: starting index of the [sub]array to search.
+* @right: ending index of the [sub]array to search.
+* @size: length of array
+* @value: value to search
+*
+* Return: -1 if not find or array null, otherwise index of element founded
+*/
+int do_binary_search(int *array, size_t left, size_t right, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	while (right >= left)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+
+	return (-1);
+}
+
+/**
 * exponential_search - Linear search function
 * @array: Pointer to integer
 * @size: length of array
@@ -25,51 +61,4 @@ int exponential_search(int *array, size_t size, int value)
 	right = i < size ? i : size - 1;
 	printf("Value found between indexes [%ld] and [%ld]\n", i / 2, right);
 	return (do_binary_search(array, i / 2, right, value));
-}
-
-/**
-* do_binary_search - Linear search function
-* @array: Pointer to integer
-* @size: length of array
-* @value: value to search
-*
-* Return: -1 if not find or array null, otherwise index of element founded
-*/
-int do_binary_search(int *array, size_t size, int value)
-{
-	int index, debut, fin, milieu;
-
-	index = -1;
-	debut = 0;
-	fin = size - 1;
-
-	if (array == NULL)
-		return (index);
-
-	while (debut <= fin)
-	{
-		print_arr(array, debut, fin);
-		milieu = (debut + fin) / 2;
-		if (*(array + milieu) == value)
-		{
-			index = milieu;
-			return (index);
-
-		}
-		else
-		{
-			if (*(array + milieu) >= value)
-			{
-				fin = milieu - 1;
-			}
-			else
-			{
-				debut = milieu + 1;
-			}
-		}
-
-	}
-
-	return (index);
-
 }
